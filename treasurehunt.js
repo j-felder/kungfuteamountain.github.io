f485c94dbc8bdaf5534a77f7acfac3beebba8bf0
Justin F <127699999+kungfuteamountain@users.noreply.github.com>
2023-07-08 15:57:45 -0700

    Add files via upload

--- treasurehunt.js
+++ treasurehunt.js
@@ -1,29 +1,25 @@
-const treasure = (location)=> { 
-   var treasureLocation = Math.floor(Math.random() * 8) 
-   var bombLocation = Math.floor(Math.random() * 8) 
-    
-    if(treasureLocation === location){
-       document.getElementById(location).innerHTML = "💰"
-       alert ("You Win!")  // experimental auto reload of game after a win next line
-     window.location = 'file:///C:/Users/Justin%20Felder/Desktop/jumpstart/index.html';
-
-    
-    } 
-    else if (bombLocation === location){
-       document.getElementById(location).innerHTML = "🔥" 
-       setTimeout(() => {
-        alert("Replay?")
-     }, 1000)
-
-       
-      alert("You lose!")  //testing reload function after losing on next line
-       window.location = 'file:///C:/Users/Justin%20Felder/Desktop/jumpstart/index.html';
-}
-    else {
-       document.getElementById(location).innerHTML = "🙈" 
-    }
-
+const treasure = (clickedItem) => {
+  if(clickedItem === goldPile) {
+    document.getElementById(clickedItem).innerHTML = "🌟"
+    countClicked = countClicked + 1
+    } else if(clickedItem === explosive) {
+      document.getElementById(clickedItem).innerHTML = "💣"
+      countClicked = countClicked + 1
+    } else {document.getElementById(clickedItem).innerHTML = "❌"
+    countClicked = countClicked + 1
   }
-//setTimeout(() => {
- // console.log("Delayed for 1 second.");
-//}, 1000)
+  document.getElementById("counter").innerHTML = `${countClicked}`
+  countRemain = countRemain - 1
+  document.getElementById("countRemain").innerHTML = `${countRemain}`
+}
+
+const goldPile = Math.floor(Math.random()* 9)
+
+var explosive = Math.floor(Math.random() * 9)
+if (explosive === goldPile) {
+  explosive = Math.floor(Math.random()* 9)
+}
+
+
+var countClicked =  0
+var countRemain = 8 
